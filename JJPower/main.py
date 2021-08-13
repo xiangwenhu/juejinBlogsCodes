44cc47afd35f53f88a5dad5337abf611478d4e3b
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-08-13 09:19:17 +0800

    数据上传

--- JJPower/main.py
+++ JJPower/main.py
@@ -47,6 +47,10 @@ def extraSummaryData(data):
         "post_article_count",  # 发文数量
         "got_digg_count",   # 获赞数
         "got_view_count",  # 阅读数
+        "total_collect_count", # 总收藏数
+        "total_comment_count", # 总评论数
+        "mdSummary",
+        "mdAv"
     ])
 
 
@@ -71,6 +75,7 @@ def downloadArticleList(uid):
                                 data=data, method="post")
         cursor += 10
         articles.extend(map(mapArticleData, res["data"]))
+        # break
     return articles
 
 
@@ -79,25 +84,44 @@ def downloadUserDatas():
         downloadUserData(uid)
 
 
-def downloadSummary(uid):
-    res = utils.getJSONData(URLS.get("summary") % (uid))
-    folder = "./JJPower/datas/%s" % uid
-    file = "./JJPower/datas/%s/%s.json" % (uid, "summary")
-    utils.ensureDir(folder)
-    # 摘要数据
-    utils.saveFile(file,  json.dumps(
-        extraSummaryData(res["data"]), ensure_ascii=False))
-
-
 def downloadUserData(uid):
     # 摘要数据
-    downloadSummary(uid)
+    summary = utils.getJSONData(URLS.get("summary") % (uid))["data"];
+    summaryfolder = "./JJPower/datas/%s" % uid
+    summaryFile = "./JJPower/datas/%s/%s.json" % (uid, "summary")
+    utils.ensureDir(summaryfolder)
+
     # 文章列表数据
     articles = downloadArticleList(uid)
     utils.saveFile("./JJPower/datas/%s/articles.json" %
                    uid, json.dumps(articles, ensure_ascii=False))
 
     sTop = summaryAndTopItems(articles)
+
+    # 总收藏数和总评论数
+    summary["total_collect_count"] = sTop["total_collect_count"]
+    summary["total_comment_count"] = sTop["total_comment_count"]
+    summary["mdSummary"] = "| %s| %s|%s |%s |%s |%s | %s|"%(
+        summary["user_name"], 
+        summary["post_article_count"], 
+        summary["got_digg_count"], 
+        summary["total_comment_count"], 
+        summary["total_collect_count"], 
+        summary["got_view_count"], 
+        summary["follower_count"], 
+    ) 
+
+    articleCount = summary["post_article_count"]
+    summary["mdAv"] = "| %s|%s |%s |%s |%s | "%(
+        summary["user_name"], 
+        round(summary["got_digg_count"] /articleCount)  , 
+        round(summary["total_comment_count"] /articleCount), 
+        round(summary["total_collect_count"] /articleCount), 
+        round(summary["got_view_count"] /articleCount), 
+    ) 
+
+    utils.saveFile(summaryFile,  json.dumps(
+        extraSummaryData(summary), ensure_ascii=False))
 
     utils.saveFile("./JJPower/datas/%s/sTop.json" %
                    uid, json.dumps(sTop, ensure_ascii=False))
@@ -111,29 +135,39 @@ def summaryAndTopItems(articles):
     for art in articles:
         collect_count += art["collect_count"]
         comment_count += art["comment_count"]
+        art["score"]  = round(art["digg_count"] * 1 + art["collect_count"] * 0.5 + art["comment_count"] * 0.2 + art["view_count"] * 0.01)
 
     # 总收藏数
     result["total_collect_count"] = collect_count
     # 总评论数
     result["total_comment_count"] = comment_count
 
-    # 点赞
+
+    # # 点赞
+    # s1 = sorted(
+    #     articles, key=lambda article: article["digg_count"], reverse=True)[0:3]
+    # result["digg_count"] = s1
+    # # 收藏
+    # s1 = sorted(
+    #     articles, key=lambda article: article["collect_count"], reverse=True)[0:3]
+    # result["collect_count"] = s1
+    # # 评论
+    # s1 = sorted(
+    #     articles, key=lambda article: article["comment_count"], reverse=True)[0:3]
+    # result["comment_count"] = s1
+    # # 阅读
+    # s1 = sorted(
+    #     articles, key=lambda article: article["view_count"], reverse=True)[0:3]
+
     s1 = sorted(
-        articles, key=lambda article: article["digg_count"], reverse=True)[0:3]
-    result["digg_count"] = s1
-    # 收藏
-    s1 = sorted(
-        articles, key=lambda article: article["collect_count"], reverse=True)[0:3]
-    result["collect_count"] = s1
-    # 评论
-    s1 = sorted(
-        articles, key=lambda article: article["comment_count"], reverse=True)[0:3]
-    result["comment_count"] = s1
-    # 阅读
-    s1 = sorted(
-        articles, key=lambda article: article["view_count"], reverse=True)[0:3]
-        
-    result["view_count"] = s1
+         articles, key=lambda article: article["score"], reverse=True)[0:6]
+    result["totalScore"] = s1
+    
+    md = "";
+    for art in s1:
+        md = md + "* [%s](https://juejin.cn/post/%s) - %s     " %(art["title"],art["article_id"], art["score"])
+    
+    result["mdTotalScore"] = md
 
     return result
 
--- JJPower/readme.md
+++ JJPower/readme.md
@@ -5,3 +5,13 @@
 ## 运行代码
 1. 下载完毕后, 进入JJPower目录
 2. `python main.py`
+
+
+## 代码语言
+
+#### 数据采集
+1. windows 环境
+2. Python 3.9.1
+#### 数据展现 - [暂未实现]
+1. Vanilla JS  
+2. echarts
